2cf5aa594d5ce781e5982b65ed8a724c70949d80
Mike Joseph <mikejoseph@Mikes-MacBook-Pro.local>
2022-11-26 15:56:14 +0200

    added navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import Navbar from '../components/navbar';
 
 function Home() {
   return (
     <Layout home>
+        <Navbar/>
       <Head>
         <title>{siteTitle}</title>
       </Head>
