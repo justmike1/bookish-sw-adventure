f02fde7f44e794950df46e369cfe62d01839cfe0
Mike Joseph <mikejoseph@Mikes-MacBook-Pro.local>
2022-11-30 12:31:58 +0200

    material ui

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,8 +11,9 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import Link from '../src/Link';
+
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Open My CV', 'Download My CV'];
@@ -41,24 +42,9 @@ function ResponsiveAppBar() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
+          <Link href="/" style={{ color: 'black' }} target="_blank">
+            <GitHubIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            </Link>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
@@ -96,7 +82,7 @@ function ResponsiveAppBar() {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <GitHubIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
@@ -112,9 +98,7 @@ function ResponsiveAppBar() {
               color: 'inherit',
               textDecoration: 'none',
             }}
-          >
-            LOGO
-          </Typography>
+          ></Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,12 @@ import Box from '@mui/material/Box';
 import Link from '../src/Link';
 import ProTip from '../src/ProTip';
 import Copyright from '../src/Copyright';
+import Navbar from '../components/navbar';
 
 export default function Home() {
   return (
     <Container maxWidth="lg">
+      <Navbar></Navbar>
       <Box
         sx={{
           my: 4,
