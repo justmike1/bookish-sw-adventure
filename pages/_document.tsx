455e721febe06a087aeb673376f5934b47663de8
Mike Joseph <mikejoseph@Mikes-MacBook-Pro.local>
2022-11-30 19:23:48 +0200

    best folder hirerchy

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
 import theme, { roboto } from '../components/Theme';
-import createEmotionCache from '../components/createEmotionCache';
+import createEmotionCache from '../components/CreateEmotionCache';
 
 export default class MyDocument extends Document {
   render() {
